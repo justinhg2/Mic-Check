a56b7f0f501ae0c290dfa307092e337d37d65602
Justin Greenberg <justinhg720@users.noreply.github.com>
2025-10-01 20:30:16 -0500

    Initial Commit

---
+++ Mic Check/Mic_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mic_CheckApp.swift
+//  Mic Check
+//
+//  Created by Justin Greenberg on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Mic_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
